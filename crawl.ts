d54f72368a947e155f4efb6b2457248e426506e0
Winter <Winter@Jonathans-MacBook-Pro.local>
2024-01-21 00:55:58 +0000

    cleanu

--- crawl.ts
+++ crawl.ts
@@ -1,21 +1,7 @@
 export function normalizeURL(url: string): string {
- //var normalizedURL = "";
  const address = new URL(url);
- console.log(address.host);
- console.log(address.pathname);
- //console.log(address.pathname.slice(-1));
- //console.log(address.pathname.slice(0, -1));
  const path = address.pathname.slice(-1) == "/" ? address.pathname.slice(0, -1) : address.pathname;
- console.log(path);
  return `${ address.host }${ path }`;
- /*address.
- if(url.slice(-1) == "/") {
-   normalizedURL = url.slice(0, -1);
- } else {
-   normalizedURL = url;
- }
- return normalizedURL;
- */
 }
 
 
